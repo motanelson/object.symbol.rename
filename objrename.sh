c1ff3f73b7aefe80657b127d7fd83f01a1270c3a
nelson jr mota <118323821+motanelson@users.noreply.github.com>
2026-05-04 22:02:52 +0100

    Delete objrename.sh

--- objrename.sh
+++
@@ -1,8 +0,0 @@
-printf "give me the file object to change ? \n"
-read a 
-printf "give me the symbol object from ? \n"
-read b
-printf "give me the new symbol object to rename ? \n"
-read c
-objcopy --redefine-sym $b=$c $a /tmp/$a
-cp  /tmp/$a $a
